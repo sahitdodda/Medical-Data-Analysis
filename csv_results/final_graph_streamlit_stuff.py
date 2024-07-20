a4bd2e6645a2b97cdab7e7d14aa3bbb2c465e981
sahitdodda <sahitd@outlook.com>
2024-07-19 20:02:25 -0400

    removed mice

--- csv_results/final_graph_streamlit_stuff.py
+++ csv_results/final_graph_streamlit_stuff.py
@@ -9,7 +9,7 @@ import re
 from sklearn.linear_model import LinearRegression
 import missingno as mno
 from sklearn.preprocessing import MinMaxScaler
-from statsmodels.imputation.mice import MICEData
+#from statsmodels.imputation.mice import MICEData
 
 import plotly.graph_objects as go
 import streamlit as st
